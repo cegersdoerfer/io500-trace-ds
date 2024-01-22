a815f98b8e49136ef5d460b55f6af5b3d079c282
cegersdoerfer <cegersdoerfer@gmail.com>
2024-01-22 08:52:59 -0500

    unpack new traces

--- unpack_all.py
+++ unpack_all.py
@@ -28,6 +28,8 @@ def unpack_all():
                 os.rename(os.path.join(folder + '/Darshan_logs_txt', darshan_log_txt), os.path.join(folder + '/Darshan_logs_txt_old', darshan_log_txt))
         # unpack each file
         for darshan_log in darshan_logs:
+            if darshan_log.endswith('.json'):
+                continue
             # unpack each file
             print("Unpacking " + darshan_log)
             print("darshan-dxt-parser --show-incomplete " + os.path.join(folder, 'Darshan_logs', darshan_log) + " > " + os.path.join(folder, 'Darshan_logs_txt', darshan_log.replace('.ini.darshan', '.txt')))
